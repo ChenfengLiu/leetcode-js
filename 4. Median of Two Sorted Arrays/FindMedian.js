b4c264163108a8536d690e2fd28b882c7b87ea90
CF <lcf.luffy@gmail.com>
2019-01-28 22:47:08 -0500

    Updated  4. Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays/FindMedian.js
+++ 4. Median of Two Sorted Arrays/FindMedian.js
@@ -24,32 +24,39 @@ The median is (2 + 3)/2 = 2.5
 * @param {number[]} nums2
 * @return {number}
 */
-var findMedianSortedArrays = function (nums1, nums2) {
-    let len = nums1.length + nums2.length;
-    let medianIndex = Math.floor(len / 2);
-    let numOfMedians = (len % 2 == 0) ? 2 : 1;
+var findMedianSortedArrays = function (A, B) {
+    let m = A.length;
+    let n = B.length;
+    if (m > n) { // to ensure m<=n
+        let temp = A; A = B; B = temp;
+        let tmp = m; m = n; n = tmp;
+    }
+    let iMin = 0, iMax = m, halfLen = Math.floor((m + n + 1) / 2);
+    while (iMin <= iMax) {
+        let i = Math.floor((iMin + iMax) / 2);
+        let j = halfLen - i;
+        if (i < iMax && B[j - 1] > A[i]) {
+            iMin = i + 1; // i is too small
+        }
+        else if (i > iMin && A[i - 1] > B[j]) {
+            iMax = i - 1; // i is too big
+        }
+        else { // i is perfect
+            let maxLeft = 0;
+            if (i == 0) { maxLeft = B[j - 1]; }
+            else if (j == 0) { maxLeft = A[i - 1]; }
+            else { maxLeft = Math.max(A[i - 1], B[j - 1]); }
+            if ((m + n) % 2 == 1) { return maxLeft; }
 
-    let i1 = 0, i2 = 0;
-    let preIndex = 0;
-    let isNums = 1;
-    let preIndexNums = 1;
-    while ((i1 + i2) !== medianIndex) {
-        if (nums1[i1] >= nums2[i2]) {
-            preIndex = i2;
-            i2++;
-            isNums = 2;
-            preIndexNums = 2;
-        } else {
-            preIndex = i1;
-            i1++;
-            isNums = 1;
-            preIndexNums = 1;
+            let minRight = 0;
+            if (i == m) { minRight = B[j]; }
+            else if (j == n) { minRight = A[i]; }
+            else { minRight = Math.min(B[j], A[i]); }
+
+            return (maxLeft + minRight) / 2;
         }
     }
-
-    return isNums === 1 ? nums1[i1] : nums2[i2];
-
-  
+    return 0;
 };
 
 
